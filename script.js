b6c59c31b49a7036e3cb67666a46029ab62506a8
keerthikavakkala <keerthikavakkala6@gmail.com>
2026-06-12 20:06:27 +0530

    script.js

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-let token = 0;
-let currentServing = 0;
+let token = localStorage.getItem("token")
+  ? parseInt(localStorage.getItem("token"))
+  : 0;
+
+let currentServing = localStorage.getItem("currentServing")
+  ? parseInt(localStorage.getItem("currentServing"))
+  : 0;
+
+document.getElementById("result").innerHTML =
+  "Token Number: " + token;
+
+document.getElementById("currentToken").innerHTML =
+  "Now Serving: Token " + currentServing;
 
 function joinQueue() {
 
-    let name = document.getElementById("name").value;
-    let service = document.getElementById("service").value;
+  let name = document.getElementById("name").value;
+  let service = document.getElementById("service").value;
 
-    if (name === "") {
-        alert("Please enter your name");
-        return;
-    }
+  if (name === "") {
+    alert("Please enter your name");
+    return;
+  }
 
-    token++;
+  token++;
 
-    let li = document.createElement("li");
-    li.innerText = "Token " + token + " - " + name + " - " + service;
+  localStorage.setItem("token", token);
 
-    document.getElementById("queueList").appendChild(li);
+  let li = document.createElement("li");
+  li.innerText =
+    "Token " + token + " - " + name + " - " + service;
 
-    document.getElementById("result").innerHTML =
-        "Token Number: " + token;
+  document.getElementById("queueList").appendChild(li);
+
+  document.getElementById("result").innerHTML =
+    "Token Number: " + token;
+
+  document.getElementById("totalCustomers").innerHTML =
+    "Total Customers: " + token;
+
+  document.getElementById("name").value = "";
 }
 
 function callNext() {
 
-    if (currentServing < token) {
-        currentServing++;
+  if (currentServing < token) {
 
-        document.getElementById("currentToken").innerHTML =
-            "Now Serving: Token " + currentServing;
-    }
+    currentServing++;
+
+    localStorage.setItem(
+      "currentServing",
+      currentServing
+    );
+
+    document.getElementById("currentToken").innerHTML =
+      "Now Serving: Token " + currentServing;
+
+  } else {
+    alert("No customers in queue");
+  }
 }
